90ed15ebf98e97ba9840d5338d11ca813124d337
Roman Hamretskyi <rgamretsky@skillwillcorp.org>
2018-03-13 16:33:48 +0200

    fix svg lib

--- android/app/src/main/java/com/frank/MainApplication.java
+++ android/app/src/main/java/com/frank/MainApplication.java
@@ -3,6 +3,11 @@ package com.frank;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.mapbox.rctmgl.RCTMGLPackage;
+import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +30,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
+            new RCTMGLPackage(),
+            new SvgPackage(),
+            new SvgPackage(),
+            new RCTMGLPackage(),
             new RCTMGLPackage(),
             new SvgPackage()
       );
